0e57224af6810da75318f62b4b066f78d3dd883c
Thpffcj <1441732331@qq.com>
2017-08-11 20:09:12 +0800

    Add methods to calculate the different sort times

--- C++/algorithms/sort/SortTestHelper.h
+++ C++/algorithms/sort/SortTestHelper.h
@@ -35,6 +35,29 @@ namespace SortTestHelper{
 
         return;
     }
+
+    template<typename T>
+    bool isSorted(T arr[], int n) {
+
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] > arr[i + 1])
+                return false;
+
+        return true;
+    }
+
+    template<typename T>
+    void testSort(const string &sortName, void (*sort)(T[], int), T arr[], int n) {
+
+        clock_t startTime = clock();
+        sort(arr, n);
+        clock_t endTime = clock();
+
+        assert(isSorted(arr, n));
+        cout << sortName << " : " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+
+        return;
+    }
 }
 
 #endif //DATASTRUCTURESANDALGORITHMS_SORTTESTHELPER_H
