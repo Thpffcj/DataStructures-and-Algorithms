9ce60507ff90ec19bacd01b084e67018a1142047
Thpffcj <1441732331@qq.com>
2017-08-15 10:00:22 +0800

    Optimize merge sorting and add quick sorting basically

---
+++ C++/algorithms/sort/MergeSort.h
@@ -0,0 +1,77 @@
+//
+// Created by Thpffcj on 2017/8/15.
+//
+
+#ifndef DATASTRUCTURESANDALGORITHMS_MERGESORT_H
+#define DATASTRUCTURESANDALGORITHMS_MERGESORT_H
+
+#include <iostream>
+#include <algorithm>
+#include "InsertionSort.h"
+
+using namespace std;
+
+// 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+template<typename  T>
+void __merge(T arr[], int l, int mid, int r){
+
+    // 经测试,传递aux数组的性能效果并不好
+    T aux[r-l+1];
+    for( int i = l ; i <= r; i ++ )
+        aux[i-l] = arr[i];
+
+    int i = l, j = mid+1;
+    for( int k = l ; k <= r; k ++ ){
+
+        if( i > mid )   { arr[k] = aux[j-l]; j ++;}
+        else if( j > r ){ arr[k] = aux[i-l]; i ++;}
+        else if( aux[i-l] < aux[j-l] ){ arr[k] = aux[i-l]; i ++;}
+        else                          { arr[k] = aux[j-l]; j ++;}
+    }
+}
+
+// 递归使用归并排序,对arr[l...r]的范围进行排序
+template<typename T>
+void __mergeSort(T arr[], int l, int r){
+
+    // 对于小规模数组,使用插入排序
+    if( r - l <= 15 ){
+        insertionSort(arr, l, r);
+        return;
+    }
+
+    int mid = (l+r)/2;
+    __mergeSort(arr, l, mid);
+    __mergeSort(arr, mid+1, r);
+    // 对于arr[mid] <= arr[mid+1]的情况,不进行merge
+    // 对于近乎有序的数组非常有效,但是对于一般情况,有一定的性能损失
+    if( arr[mid] > arr[mid+1] )
+        __merge(arr, l, mid, r);
+}
+
+//自底向上归并排序,适用链表
+template <typename T>
+void mergeSortBU(T arr[], int n){
+
+//    for( int sz = 1; sz <= n ; sz += sz )
+//        for( int i = 0 ; i < n ; i += sz+sz )
+//            // 对 arr[i...i+sz-1] 和 arr[i+sz...i+2*sz-1] 进行归并
+//            __merge(arr, i, i+sz-1, min(i+sz+sz-1,n-1) );
+
+    // Merge Sort Bottom Up 优化
+    for( int i = 0 ; i < n ; i += 16 )
+        insertionSort(arr,i,min(i+15,n-1));
+
+    for( int sz = 16; sz <= n ; sz += sz )
+        for( int i = 0 ; i < n - sz ; i += sz+sz )
+            if( arr[i+sz-1] > arr[i+sz] )
+                __merge(arr, i, i+sz-1, min(i+sz+sz-1,n-1) );
+}
+
+template<typename T>
+void mergeSort(T arr[], int n){
+
+    __mergeSort( arr , 0 , n-1 );
+}
+
+#endif //DATASTRUCTURESANDALGORITHMS_MERGESORT_H
--- C++/algorithms/sort/main.cpp
+++ C++/algorithms/sort/main.cpp
@@ -5,84 +5,62 @@
 #include "SortTestHelper.h"
 #include "SelectionSort.h"
 #include "InsertionSort.h"
+#include "MergeSort.h"
 #include "BubbleSort.h"
 #include "ShellSort.h"
 
 using namespace std;
 
-// 将arr[l...mid]和arr[mid+1...r]两部分进行归并
-template<typename  T>
-void __merge(T arr[], int l, int mid, int r){
+// 对arr[l...r]部分进行partition操作
+// 返回p,使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
+template <typename T>
+int __partition(T arr[], int l, int r){
 
-    // 经测试,传递aux数组的性能效果并不好
-    T aux[r-l+1];
-    for( int i = l ; i <= r; i ++ )
-        aux[i-l] = arr[i];
+    T v = arr[l];
 
-    int i = l, j = mid+1;
-    for( int k = l ; k <= r; k ++ ){
+    int j = l; // arr[l+1...j] < v ; arr[j+1...i) > v
+    for( int i = l + 1 ; i <= r ; i ++ )
+        if( arr[i] < v ){
+            j ++;
+            swap( arr[j] , arr[i] );
+        }
 
-        if( i > mid )   { arr[k] = aux[j-l]; j ++;}
-        else if( j > r ){ arr[k] = aux[i-l]; i ++;}
-        else if( aux[i-l] < aux[j-l] ){ arr[k] = aux[i-l]; i ++;}
-        else                          { arr[k] = aux[j-l]; j ++;}
-    }
+    swap( arr[l] , arr[j]);
+
+    return j;
 }
 
-// 递归使用归并排序,对arr[l...r]的范围进行排序
-template<typename T>
-void __mergeSort(T arr[], int l, int r){
+// 对arr[l...r]部分进行快速排序
+template <typename T>
+void __quickSort(T arr[], int l, int r){
 
-    // 对于小规模数组,使用插入排序
-    if( r - l <= 15 ){
-        insertionSort(arr, l, r);
+    if( l >= r )
         return;
-    }
 
-    int mid = (l+r)/2;
-    __mergeSort(arr, l, mid);
-    __mergeSort(arr, mid+1, r);
-    // 对于arr[mid] <= arr[mid+1]的情况,不进行merge
-    // 对于近乎有序的数组非常有效,但是对于一般情况,有一定的性能损失
-    if( arr[mid] > arr[mid+1] )
-        __merge(arr, l, mid, r);
+    int p = __partition(arr, l, r);
+    __quickSort(arr, l, p-1 );
+    __quickSort(arr, p+1, r);
 }
 
-template<typename T>
-void mergeSort(T arr[], int n){
+template <typename T>
+void quickSort(T arr[], int n){
 
-    __mergeSort( arr , 0 , n-1 );
+    __quickSort(arr, 0, n-1);
 }
 
 int main() {
 
-    int n = 50000;
+    int n = 100000;
 
-    // 测试1 一般性测试
     cout<<"Test for Random Array, size = "<<n<<", random range [0, "<<n<<"]"<<endl;
     int* arr1 = SortTestHelper::generateRandomArray(n,0,n);
-    int* arr2 = SortTestHelper::copyIntArray(arr1, n);
+    int* arr2 = SortTestHelper::copyIntArray(arr1,n);
 
-    SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
-    SortTestHelper::testSort("Merge Sort",     mergeSort,     arr2, n);
+    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
 
     delete[] arr1;
     delete[] arr2;
-
-    cout<<endl;
-
-
-    // 测试2 测试近乎有序的数组
-    int swapTimes = 100;
-    cout<<"Test for Random Nearly Ordered Array, size = "<<n<<", swap time = "<<swapTimes<<endl;
-    arr1 = SortTestHelper::generateNearlyOrderedArray(n,swapTimes);
-    arr2 = SortTestHelper::copyIntArray(arr1, n);
-
-    SortTestHelper::testSort("Insertion Sort", insertionSort, arr1, n);
-    SortTestHelper::testSort("Merge Sort",     mergeSort,     arr2, n);
-
-    delete(arr1);
-    delete(arr2);
 
     return 0;
 }
