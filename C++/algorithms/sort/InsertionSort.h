686a3ce5cb5ccbf1879302ab1cdf52832dc9f156
Thpffcj <1441732331@qq.com>
2017-08-15 09:24:29 +0800

    add merge sort

--- C++/algorithms/sort/InsertionSort.h
+++ C++/algorithms/sort/InsertionSort.h
@@ -38,4 +38,20 @@ void insertionSort(T arr[], int n){
     return;
 }
 
+// 对arr[l...r]范围的数组进行插入排序
+template<typename T>
+void insertionSort(T arr[], int l, int r){
+
+    for( int i = l+1 ; i <= r ; i ++ ) {
+
+        T e = arr[i];
+        int j;
+        for (j = i; j > l && arr[j-1] > e; j--)
+            arr[j] = arr[j-1];
+        arr[j] = e;
+    }
+
+    return;
+}
+
 #endif //DATASTRUCTURESANDALGORITHMS_INSERTIONSORT_H
