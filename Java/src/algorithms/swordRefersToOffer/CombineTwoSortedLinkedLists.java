e57ad6e8020741d308c173d86e5454621b89b1cf
Thpffcj <1441732331@qq.com>
2019-09-15 13:22:51 +0800

    有树就用递归

--- Java/src/algorithms/swordRefersToOffer/CombineTwoSortedLinkedLists.java
+++ Java/src/algorithms/swordRefersToOffer/CombineTwoSortedLinkedLists.java
@@ -11,5 +11,25 @@ public class CombineTwoSortedLinkedLists {
 
     public ListNode Merge(ListNode list1,ListNode list2) {
 
+        ListNode result = new ListNode(-1);
+        ListNode current = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                current.next = list2;
+                current = current.next;
+                list2 = list2.next;
+            } else {
+                current.next = list1;
+                current = current.next;
+                list1 = list1.next;
+            }
+        }
+        if (list1 != null) {
+            current.next = list1;
+        }
+        if (list2 != null) {
+            current.next = list2;
+        }
+        return result.next;
     }
 }
