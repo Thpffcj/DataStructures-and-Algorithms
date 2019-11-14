eafa8c78770078c671d3fb292d3a95ec5bb4a134
Thpffcj <1441732331@qq.com>
2019-11-14 14:28:49 +0800

    每天一算法，快乐每一天

---
+++ Java/src/algorithms/leetcodecn/stack/TheKthLargestElementInTheArray.java
@@ -0,0 +1,64 @@
+package algorithms.leetcodecn.stack;
+
+/**
+ * Created by thpffcj on 2019/11/13.
+ */
+
+/**
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ *
+ * 示例 1:
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ *
+ * 示例 2:
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ *
+ * 说明:
+ * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+ */
+public class TheKthLargestElementInTheArray {
+
+    public int findKthLargest(int[] nums, int k) {
+
+        int[] digits = new int[nums.length + 1];
+        digits[0] = -1;
+        for (int i = 0 ; i < nums.length; i++) {
+            digits[i + 1] = nums[i];
+        }
+
+        // 构建堆
+        int n = digits.length - 1;
+        for (int i = n / 2; i >= 1; i--) {
+            sink(digits, i, n);
+        }
+
+        // 找到第K大元素
+        for (int i = 0; i < k - 1; i++) {
+            int temp = digits[1];
+            digits[1] = digits[n];
+            digits[n] = temp;
+            n--;
+            sink(digits, 1, n);
+        }
+        return digits[1];
+    }
+
+    public void sink(int[] digits, int k, int n) {
+        while (2 * k <= n) {
+            int max = 2 * k;
+            if (2 * k + 1 <= n) {
+                if (digits[2 * k] < digits[2 * k + 1]) {
+                    max = 2 * k + 1;
+                }
+            }
+            if (digits[k] < digits[max]) {
+                int temp = digits[k];
+                digits[k] = digits[max];
+                digits[max] = temp;
+            }
+            k = max;
+        }
+    }
+}
--- Java/src/algorithms/leetcodecn/stack/TopKHighFrequencyElements.java
+++ Java/src/algorithms/leetcodecn/stack/TopKHighFrequencyElements.java
@@ -1,7 +1,60 @@
 package algorithms.leetcodecn.stack;
 
 /**
- * Created by thpffcj on 2019/10/13.
+ * Created by thpffcj on 2019/11/14.
  */
-public enum TopKHighFrequencyElements {
+
+import java.util.*;
+
+/**
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ *
+ * 示例 1:
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ *
+ * 示例 2:
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ *
+ * 说明：
+ * 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+ * 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+ */
+public class TopKHighFrequencyElements {
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        List<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length <= 0) {
+            return result;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        /**
+         * TODO Java Map按值排序的常见思路是：
+         * 将map中的entry放到List中
+         * 对List中的entry通过比较器按值排序
+         * 将排序后的entry放到map中
+         */
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        for (Map.Entry<Integer, Integer> entry : list) {
+            result.add(entry.getKey());
+        }
+        return result.subList(0, k);
+    }
 }
