0f05d33b151767434ddc9d24a457c385549504b0
Thpffcj <1441732331@qq.com>
2020-01-12 09:07:35 +0800

    假期刷题

--- Java/src/algorithms/leetcodecn/string/RegularExpressionMatching.java
+++ Java/src/algorithms/leetcodecn/string/RegularExpressionMatching.java
@@ -86,6 +86,53 @@ public class RegularExpressionMatching {
         return false;
     }
 
+    /**
+     * 回溯
+     * 如果模式串中有星号，它会出现在第二个位置，即 pattern[1] 。这种情况下，我们可以直接忽略模式串中这一部分，或者删除匹配串
+     * 的第一个字符，前提是它能够匹配模式串当前位置字符，即 pattern[0] 。如果两种操作中有任何一种使得剩下的字符串能匹配，那么初
+     * 始时，匹配串和模式串就可以被匹配。
+     */
+    public boolean isMatch2(String text, String pattern) {
+        if (pattern.isEmpty()) {
+            return text.isEmpty();
+        }
+
+        boolean firstMatch = (!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            return (isMatch(text, pattern.substring(2)) ||
+                    (firstMatch && isMatch(text.substring(1), pattern)));
+        } else {
+            return firstMatch && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+
+    /**
+     * 动态规划
+     * 因为题目拥有 最优子结构 ，一个自然的想法是将中间结果保存起来。
+     * 我们通过用 dp(i,j) 表示 text[i:] 和 pattern[j:] 是否能匹配。我们可以用更短的字符串匹配问题来表示原本的问题。
+     */
+    public boolean isMatch3(String text, String pattern) {
+
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+
+        for (int i = text.length(); i >= 0; i--){
+            for (int j = pattern.length() - 1; j >= 0; j--){
+                boolean firstMatch = (i < text.length() &&
+                        (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.'));
+
+                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                    dp[i][j] = dp[i][j+2] || firstMatch && dp[i+1][j];
+                } else {
+                    dp[i][j] = firstMatch && dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
         RegularExpressionMatching r = new RegularExpressionMatching();
         r.isMatch("ab", ".*c");
--- Java/src/algorithms/swordRefersToOffer/AdjustTheOrderOfTheArray.java
+++ Java/src/algorithms/swordRefersToOffer/AdjustTheOrderOfTheArray.java
@@ -2,9 +2,7 @@ package algorithms.swordRefersToOffer;
 
 /**
  * Created by thpffcj on 2019/9/4.
- */
-
-/**
+ *
  * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分，并保证奇数和奇
  * 数，偶数和偶数之间的相对位置不变。
  */
@@ -14,7 +12,8 @@ public class AdjustTheOrderOfTheArray {
         // 相对位置不变，稳定性
         // 插入排序的思想
         int m = array.length;
-        int k = 0;  // 记录已经摆好位置的奇数的个数
+        // 记录已经摆好位置的奇数的个数
+        int k = 0;
         for (int i = 0; i < m; i++) {
             if (array[i] % 2 == 1) {
                 int j = i;
